28d9343e9d5504e1686fc7a8ffeb10efe884f830
Pandi <fandy.setiadi90@gmail.com>
2026-07-21 08:42:32 +0700

    update validate pengguna

--- app/Http/Controllers/PenggunaController.php
+++ app/Http/Controllers/PenggunaController.php
@@ -27,13 +27,13 @@ class PenggunaController extends Controller
     {
         $request->validate([
             'email' => 'required|email|unique:pengguna,email',
-            'nama' => ['required', 'regex:/^[a-zA-Z\s]+$/'],
+            'nama' => ['required', 'regex:/^[a-zA-Z\s\'.-,]+$/'],
             'nomor_induk' => ['required', 'numeric'],       
             'jabatan' => 'required|in:Admin,Kepala Sekolah,Wali Kelas',
             'poto' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
             ], [
                 'nama.required' => 'Nama wajib diisi.',
-                'nama.regex' => 'Nama hanya boleh berisi huruf dan spasi.',
+                'nama.regex' => 'Nama hanya boleh berisi huruf, spasi, dan tanda baca.',    
                 'nomor_induk.required' => 'Nomor Induk wajib diisi.',
                 'nomor_induk.numeric' => 'Nomor Induk hanya boleh berisi angka.',
             
@@ -100,13 +100,13 @@ class PenggunaController extends Controller
 
         $request->validate([
             'email' => 'required|email|unique:pengguna,email,' . $id . ',id_pengguna',
-            'nama' => ['required', 'regex:/^[a-zA-Z\s]+$/'],
+            'nama' => ['required', 'regex:/^[a-zA-Z\s\'.-,]+$/'],
             'nomor_induk' => ['required', 'numeric'],       
             'jabatan' => 'required|in:Admin,Kepala Sekolah,Wali Kelas',
             'poto' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
             ], [
                 'nama.required' => 'Nama wajib diisi.',
-                'nama.regex' => 'Nama hanya boleh berisi huruf dan spasi.',
+                'nama.regex' => 'Nama hanya boleh berisi huruf, spasi, dan tanda baca.',        
                 'nomor_induk.required' => 'Nomor Induk wajib diisi.',
                 'nomor_induk.numeric' => 'Nomor Induk hanya boleh berisi angka.',
                 
